687704db7d0e8b8472041d380333570dea54c326
ALEXLAPTOP\pison <pisonalex@gmail.com>
2024-10-28 16:01:20 +0100

    String copy implemented

--- Q4/q4.3.c
+++ Q4/q4.3.c
@@ -1,2 +1,25 @@
 // Written by api on 28/10/2024
-// This program is my implementation of stringCopy function
+// This program is my implementation of stringCopy function - from COMP0002 slides
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *stringCopy(char *s)
+{
+    char *to = malloc(strlen(s) * sizeof(char));
+    int i = 0;
+    while (to[i] = s[i])
+    {
+        i++;
+    }
+    return to;
+}
+
+int main(void)
+{
+    char *to = stringCopy("hello");
+    printf("%s\n", to);
+    free(to);
+    return 0;
+}
