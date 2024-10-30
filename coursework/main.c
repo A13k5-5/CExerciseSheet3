0cb5a8a0b8da69b93533dc9026450236bf61138b
Alex Pison <pisonalex@gmail.com>
2024-10-30 14:45:31 +0100

    there is a wall that surrounds the grid

--- coursework/main.c
+++ coursework/main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// 'v' - visited, 'o' - not visited, 'm' - marker, 'w' - wall
+
 void printMap(char **map, int width, int height)
 {
     for (int i = 0; i < height; i++)
@@ -27,15 +29,34 @@ char **generateMap(int width, int height)
     {
         for (int j = 0; j < width; j++)
         {
-            map[i][j] = 'a';
+            map[i][j] = 'o';
         }
     }
     return map;
 }
 
+void generateWall(char **map, int width, int height)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+            {
+                map[i][j] = 'w';
+            }
+        }
+    }
+}
+
 int main(void)
 {
-    char **map = generateMap(3, 4);
-    printMap(map, 3, 4);
+    int width = 8;
+    int height = 8;
+    char **map = generateMap(width, height);
+    printMap(map, width, height);
+    printf("\n");
+    generateWall(map, width, height);
+    printMap(map, width, height);
     return 0;
 }
